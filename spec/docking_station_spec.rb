9026f8ccd6095e7cfda3caff07c6414fdee6ae7a
Henry Stanley <henry@henrystanley.com>
2014-08-11 17:27:04 +0100

    Added failing DockingStation test (last commit failed)

---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,11 @@
+describe DockingStation do 
+	let(:bike) {Bike.new}
+	let(:station) {DockingStation.new}
+
+	it "should accept a bike" do
+		expect(station.bike_count) to eq(0)
+		station.dock(bike)
+		expect(station.bike_count) to eq(1)
+	end
+	
+end
