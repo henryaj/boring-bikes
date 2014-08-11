0cfd668bccf5fcb4ed8c0e072cf4868d821d7327
Henry Stanley <henry@henrystanley.com>
2014-08-11 15:51:54 +0100

    Updated to pass bike broken test

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,6 +1,8 @@
+require 'bike'
+
 describe Bike do 
 	let(:the_bike) {Bike.new}
 	it "should not be broken after we create it" do
-		expect(the_bike).not to be_broken
+		expect(the_bike).not_to be_broken
 	end	
 end
