609a81c9af816ef162d47b8253098985c0b6ec6b
Henry Stanley <henry@henrystanley.com>
2014-08-11 15:53:27 +0100

    Added failing should be able to break bike test

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,8 +1,13 @@
 require 'bike'
 
 describe Bike do 
-	let(:the_bike) {Bike.new}
+	let(:bike) {Bike.new}
 	it "should not be broken after we create it" do
-		expect(the_bike).not_to be_broken
+		expect(bike).not_to be_broken
 	end	
+
+	it "should be able to break" do
+		bike.break!
+		expect(bike).to be_broken
+	end
 end
