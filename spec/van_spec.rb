385209605f223d3500b85574d719fdd0ca9865f3
David Wickes <dave.wickes@gmail.com>
2014-08-12 15:19:32 +0100

    passes van capacity init test, written a test to send a van to a docking station

--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -1,9 +1,16 @@
 require 'van'
+require 'docking_station'
 
 describe Van do
 	let(:van) {Van.new(:capacity => 25)}
+	let(:station) { DockingStation.new }
 
 	it "it has the capacity it is initialized with" do
 		expect(van.capacity).to eq(25)
+	end
+
+	it "should be able to go to a docking station" do
+		van.go_to(station)
+		expect(van.location).to eq station
 	end
 end
