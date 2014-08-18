c7c3d16176112e3e77f2f31f7d8ca0e6afd96aa8
ruthearle <rrearle@gmail.com>
2014-08-18 13:41:54 +0100

    Changed bike to :bike in let statement

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -2,9 +2,9 @@ require 'person'
 
 describe 'a person' do
 
+  let(:bike)             { double :bike, break!: :bike }
   let(:person)           { Person.new                 }
   let(:person_with_bike) { Person.new(bike)          }
-  let(:bike)             { double :bike, break!: bike }
 
 	it 'starts off without a bike' do
 		expect(person.has_bike?).to eq false
@@ -12,16 +12,17 @@ describe 'a person' do
 
   it 'can have a bike' do
     person_with_bike
-    expect(person.has_bike?).to eq true
+    expect(person.has_bike?).to be true
   end
 
 	it 'can have a horrible accident' do
     person_with_bike
-		expect(person.has_accident!).to eq person
+    expect(person.has_accident!).to eq person
 	end
 
   it 'can break a bike when it has an accident' do
     person_with_bike
     expect(:bike).to receive(:break!)
+    person.has_accident!
   end
 end
