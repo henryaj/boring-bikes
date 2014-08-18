730838be20dd435c916f7011fb7f437cd38b0183
ruthearle <rrearle@gmail.com>
2014-08-18 12:48:44 +0100

    added test for person to have a bike. failed

---
+++ lib/person.rb
@@ -0,0 +1,6 @@
+class Person
+
+    def has_bike?
+      false
+    end
+end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -4,6 +4,11 @@ describe 'a person' do
 
 	it 'starts off without a bike' do
 		person = Person.new
-		expect(person.has_bike?).to be false
+		expect(person.has_bike?).to eq false
 	end
+
+  it 'can have a bike' do
+    person = Person.new(:bike)
+    expect(person.has_bike?).to eq true
+  end
 end
