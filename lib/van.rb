c9fe7bf506165c0c0433d73fc6b8cdd9a638e4c7
David Wickes <dave.wickes@gmail.com>
2014-08-19 01:19:07 +0100

    van up and running - looks like it shares a number of methods with docking station so aiming to refactor

--- lib/van.rb
+++ lib/van.rb
@@ -1,6 +1,16 @@
 class Van
+	def initialize
+		@bikes = []
+	end
 	
 	def dock(bike)
+		@bikes << bike
 		self
 	end
+
+	def release_bike
+		bike = @bikes.select(&:working?).pop
+		@bikes.delete(bike)
+	end
+	
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -1,9 +1,24 @@
 require 'van'
 
 describe 'a van' do
+	let(:working_bike) { double :bike, working?: true }
+	let(:bike) { double :bike }
+	let(:van) { Van.new }
+
 	it "can dock a bike" do
-		van = Van.new
-		bike = double :bike
 		expect(van.dock(bike)).to eq van
+	end
+
+	it "can release a bike" do
+		van.dock(working_bike)
+		expect(van.release_bike).to eq working_bike
+	end
+
+	it "only releases working bikes" do
+		broken_bike = double :bike, working?: false
+		van.dock(broken_bike)
+		van.dock(working_bike)
+		van.dock(working_bike)
+		expect(van.release_bike).to be working_bike
 	end
 end
