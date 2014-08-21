7ffc2a359aa950b9d1de881027e2e6a5e7a7bf68
Jerome Pratt <jerome.pratt@outlook.com>
2014-08-21 15:49:40 +0100

    Adding a test for garage fixing broken bikes

--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -2,15 +2,25 @@ require 'garage'
 
 describe Garage do
 
+let(:garage) {Garage.new}
+let(:van) {double :van, dump_broken_bikes: nil}
+let(:bike) {double :bike, class: Bike}
+let(:broken_bike) {double :bike}
+let(:working_bike) {double :bike, broken?: false}
+
 	it_behaves_like 'a bike container'
 
 	it 'can receive a lot of broken bikes in one go' do
-		garage = Garage.new
-		bike = double :bike
-		van = double :van
 		expect(van).to receive(:dump_broken_bikes).and_return([bike,bike,bike])
 		expect(garage.receive_bikes_from(van)).to be nil
 	end
+
+	it "it can fix broken bikes" do
+		expect(bike).to receive(:fix!)
+		garage.dock_bike(bike)
+		garage.fix!
+	end
+
 
 
 
